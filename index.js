cdc6644282b7e397262f28c6c7bb97b40bd0ae38
Deric Walintukan <deric@loopapps.com>
2022-08-01 16:38:33 -0700

    update business logic to send slack message

--- index.js
+++ index.js
@@ -14,31 +14,6 @@ const getInput = (key) => {
 }
 
 /**
- * Gets the issue.
- * @param {object} octokit Octokit
- * @returns {object} Issue
- */
-const getIssue = async (octokit) => {
-  const { owner, repo } = github.context.repo
-  const { number: issueNumber } = github.context.issue
-  const { data: issue } = await octokit.rest.issues.get({
-    owner,
-    repo,
-    issue_number: issueNumber
-  })
-  return issue
-}
-
-/**
- * Gets the labels for a given issue.
- * @param {array} labels Issue labels
- * @returns {array} List of label names
- */
- const getLabels = (labels) => {
-  return labels.map((label) => label.name)
-}
-
-/**
  * Posts to Slack via webhook.
  * @param {object} body Body to post to Slack
  */
@@ -48,144 +23,42 @@ const getIssue = async (octokit) => {
 }
 
 /**
- * Validates if the issue is a Release Candidate.
- * @param {array} labels List of label names for the issue
+ * Constructs the Slack message JSON payload.
+ * @param {string} header Slack message header
+ * @param {string} body Slack message body
  */
-const validateReleaseCandidateIssue = (labels) => {
-  if (!labels.includes('RC')) throw Error('Issue does not have "RC" label')
-}
-
-/**
- * Parses the issue body and gets the tag and branch.
- * @param {object} body Issue body
- * @returns {object} Tag and branch
- */
-const parseIssueBody = (body) => {
-  // Extract release tag with regex
-  const tagRegex = new RegExp(/-\sRelease\stag:\s(v\d{8}\.\d(-hotfix)*)/)
-  const tagMatch = body.match(tagRegex)
-  if (!tagMatch) throw Error(`No "Release tag" found in issue body:\n${body}`)
-  const tag = tagMatch[1]
-
-  // Extract branch with regex
-  const branchRegex = new RegExp(/-\sBranch:\s((release|hotfix)\/v\d{8}\.\d(-hotfix)*)/)
-  const branchMatch = body.match(branchRegex)
-  if (!branchMatch) throw Error(`No "Branch" found in issue body:\n${body}`)
-  const branch = branchMatch[1]
-
-  return { tag, branch }
-}
-
-/**
- * Handles release signed off.
- * @param {array} labels List of labels
- * @param {object} octokit Octokit
- * @param {string} tag Tag that will be used for the release
- * @param {string} branch Branch that will be used for the release
- * @returns {string} Release URL on Github
- */
-const handleReleaseSignedOff = async (labels, octokit, tag, branch) => {
-  if (labels.includes('QA Approved')) {
-    const { owner, repo } = github.context.repo
-    
-    // Create the release
-    const { data: { html_url: releaseUrl } } = await octokit.rest.repos.createRelease({
-      owner,
-      repo,
-      name: tag,
-      tag_name: tag,
-      draft: false,
-      target_commitish: branch
-    })
-
-    // Post success message
-    await postToSlack({
-      "blocks": [
-        {
-          "type": "header",
-          "text": {
-            "type": "plain_text",
-            "text": `[${tag}] Release/Hotfix approved ✅`
-          }
-        },
-        {
-          "type": "section",
-          "text": {
-            "type": "mrkdwn",
-            "text": `\`${branch}\` is approved for publishing.`
-          },
-          "accessory": {
-            "type": "button",
-            "text": {
-              "type": "plain_text",
-              "text": "Go"
-            },
-            "url": `${releaseUrl}`,
-            "action_id": "button-action"
-          }
+const constructSlackMessage = (header, body) => {
+  return {
+    "blocks": [
+      {
+        "type": "header",
+        "text": {
+          "type": "plain_text",
+          "text": header
         }
-      ]
-    })
-  }
-}
-
-/**
- * Handles cancelling the release.
- * @param {array} labels List of labels
- * @param {object} octokit Octokit
- * @param {string} tag Tag that was meant to be released
- * @param {string} branch Release branch that will be deleted
- */
-const handleReleaseCancelled = async (labels, octokit, tag, branch) => {
-  if (!labels.includes('QA Approved')) {
-    const { owner, repo } = github.context.repo
-
-    // Delete Release Candidate branch
-    await octokit.rest.git.deleteRef({
-      owner,
-      repo,
-      ref: `heads/${branch}`
-    })
-
-    // Post cancelled message
-    await postToSlack({
-      "blocks": [
-        {
-          "type": "header",
-          "text": {
-            "type": "plain_text",
-            "text": `[${tag}] Release/Hotfix cancelled ❌`
-          }
-        },
-        {
-          "type": "section",
-          "text": {
-            "type": "mrkdwn",
-            "text": `\`${branch}\` was cancelled.`
-          }
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": body
         }
-      ]
-    })
+      }
+    ]
   }
 }
 
 const run = async () => {
   try {
-    // Get token and init
-    const token = getInput('workflow-token')
-    const octokit = github.getOctokit(token)
-    const issue = await getIssue(octokit)
-    const labels = getLabels(issue.labels)
+    // Get inputs
+    const messageHeader = getInput('slack-message-header')
+    const messageBody = getInput('slack-message-body')
 
-    // Validates if the issue is a Release Candidate
-    validateReleaseCandidateIssue(labels)
-    const { tag, branch } = parseIssueBody(issue.body)
+    // Construct the message payload
+    const message = constructSlackMessage(messageHeader, messageBody)
 
-    // Handle release signed off
-    await handleReleaseSignedOff(labels, octokit, tag, branch)
-
-    // Handle release cancelled
-    await handleReleaseCancelled(labels, octokit, tag, branch)
+    // Post to Slack
+    await postToSlack(message)
   } catch (error) {
     core.setFailed(error.message)
   }
